4daf654b4f7287350bf1a92613ead9401055f046
supremeincubator <mohammedshamil0300@gmail.com>
2023-02-05 20:25:30 +0530

    added hover animation

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -59,7 +59,7 @@ const Weather = ({ cityCode, units, api, index }) => {
     fetchWeather
   );
 
-   console.log({ isLoading, isFetching, data, isSuccess });
+  console.log({ isLoading, isFetching, data, isSuccess });
 
   useEffect(() => {
     if (data) {
@@ -115,71 +115,73 @@ const Weather = ({ cityCode, units, api, index }) => {
 
   return (
     <>
-      <div className={`children`}>
-        <div className="innerFlex">
-          <div className="bgImg">
-            <div className={`flexChild innerGrid color-${index}`}>
-              <p className={`grid grid-1`}>
-                {data.name}, {data.sys.country},
-              </p>
-              <p className="grid grid-2">{Math.round(data.main.temp)}°c</p>
-              <p className="grid grid-3">{dateTime}</p>
-              <div className="grid grid-4">
-                <div className="icon weatherIcon">
-                  <img src={icon} /* alt={mist} */ />
+      <a>
+        <div className={`children`}>
+          <div className="innerFlex">
+            <div className="bgImg">
+              <div className={`flexChild innerGrid color-${index}`}>
+                <p className={`grid grid-1`}>
+                  {data.name}, {data.sys.country},
+                </p>
+                <p className="grid grid-2">{Math.round(data.main.temp)}°c</p>
+                <p className="grid grid-3">{dateTime}</p>
+                <div className="grid grid-4">
+                  <div className="icon weatherIcon">
+                    <img src={icon} /* alt={mist} */ />
+                  </div>
+                  <span>
+                    <p>{data.weather[0].description}</p>
+                  </span>
                 </div>
-                <span>
-                  <p>{data.weather[0].description}</p>
-                </span>
+                <p className="grid grid-6">
+                  Temp Min: {Math.round(data.main.temp_min)}°C
+                </p>
+                <p className="grid grid-7">
+                  Temp Max: {Math.round(data.main.temp_max)}°C
+                </p>
               </div>
-              <p className="grid grid-6">
-                Temp Min: {Math.round(data.main.temp_min)}°C
-              </p>
-              <p className="grid grid-7">
-                Temp Max: {Math.round(data.main.temp_max)}°C
-              </p>
+            </div>
+          </div>
+          <div style={{ pointerEvents: "none" }}>
+            <div className="flexChild singleColor">
+              <div>
+                <p>
+                  <b>Pressure: </b> {data.main.pressure}Pa
+                </p>
+                <p>
+                  <b>Humidity: </b> {data.main.humidity}%
+                </p>
+                <p>
+                  <b>Visibility: </b> {(data.visibility / 1000).toFixed(1)}
+                  km
+                </p>
+              </div>
+              <div className="windParent">
+                <div className="wind">
+                  <span className="icon">
+                    <img src={arrow} alt="arrow" />
+                  </span>
+                  <span>
+                    <p>
+                      {data.wind.speed} m/s {data.wind.deg} Degree
+                    </p>
+                  </span>
+                </div>
+              </div>
+              <div className="sunParent">
+                <p>
+                  <b>Sunrise: </b>
+                  {sunrise}
+                </p>
+                <p>
+                  <b>Sunset: </b>
+                  {sunset}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div>
-          <div className="flexChild singleColor">
-            <div>
-              <p>
-                <b>Pressure: </b> {data.main.pressure}Pa
-              </p>
-              <p>
-                <b>Humidity: </b> {data.main.humidity}%
-              </p>
-              <p>
-                <b>Visibility: </b> {(data.visibility / 1000).toFixed(1)}
-                km
-              </p>
-            </div>
-            <div className="windParent">
-              <div className="wind">
-                <span className="icon">
-                  <img src={arrow} alt="arrow" />
-                </span>
-                <span>
-                  <p>
-                    {data.wind.speed} m/s {data.wind.deg} Degree
-                  </p>
-                </span>
-              </div>
-            </div>
-            <div className="sunParent">
-              <p>
-                <b>Sunrise: </b>
-                {sunrise}
-              </p>
-              <p>
-                <b>Sunset: </b>
-                {sunset}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      </a>
     </>
   );
 };
